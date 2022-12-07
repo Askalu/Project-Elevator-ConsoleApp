03811aba46c657b5ef30985098fa451cbdd3b9ec
Biniam Kiflay <biniamale@gmail.com>
2022-12-07 07:43:08 +0100

    The Elevator device is creating and needs some functionalities.

--- ElevatorConsoleApp/Program.cs
+++ ElevatorConsoleApp/Program.cs
@@ -1,158 +1,432 @@
-﻿
-namespace ElevatorConsoleApp
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Elevator
 {
-    public class Program
+    private Direction currentDirection = Direction.UP;
+    private State currentState = State.IDLE;
+    private int currentFloor = 0;
+
+    Stack<Request> currentJobs = new Stack<Request>();
+    Stack<Request> upPendingJobs = new Stack<Request>();
+    Stack<Request> downPendingJobs = new Stack<Request>();
+    public void startElevator()
     {
-        private const string QUIT = "q";
-
-        public static void Main(string[] args)
+        Console.WriteLine("Welcome");
+        Console.WriteLine("The Elevator has started functioning");
+        while (true)
         {
-
-        Start:
-            Console.WriteLine("Welcome");
-            Console.WriteLine("How many floors does this elevator will be needed?");
-
-            int floor; string floorInput; Elevator elevator;
-
-            floorInput = Console.ReadLine();
-
-            if (Int32.TryParse(floorInput, out floor))
-                elevator = new Elevator(floor);
-            else
+            if (checkIfJob())
             {
-                Console.WriteLine("That' doesn't make sense...");
-                Console.Beep();
-                Thread.Sleep(5000);
-                Console.Clear();
-                goto Start;
-            }
-            string input = string.Empty;
-
-            while (input != QUIT)
-            {
-                Console.WriteLine("Please press which floor you would like to go to");
-
-                input = Console.ReadLine();
-                if (Int32.TryParse(input, out floor))
-                    elevator.FloorPress(floor);
-                else if (input == QUIT)
-                    Console.WriteLine("GoodBye!");
-                else
-                    Console.WriteLine("You have pressed an incorrect floor, Please try again");
+                if (currentDirection == Direction.UP)
+                {
+                    Request request = currentJobs.Pop();
+                    processUpRequest(request);
+                    if (currentJobs.Count == 0)
+                    {
+                        addPendingDownJobsToCurrentJobs();
+                    }
+                }
+                if (currentDirection == Direction.DOWN)
+                {
+                    Request request = currentJobs.Pop();
+                    processDownRequest(request);
+                    if (currentJobs.Count == 0)
+                    {
+                        addPendingUpJobsToCurrentJobs();
+                    }
+                }
             }
         }
     }
-
-
-    public class Elevator
+    public bool checkIfJob()
     {
-        //The building building may has x numbers of floors
-
-        private bool[] floorReady;
-        public int CurrentFloor = 0;
-        private int topfloor;
-        public State Status = State.STOPPED;
-
-        public Elevator(int NumberOfFloors = 5)
+        if (currentJobs.Count == 0)
         {
-            floorReady = new bool[NumberOfFloors + 1];
-            topfloor = NumberOfFloors;
-
+            return false;
         }
-
-        private void Stop(int floor)
+        return true;
+    }
+    public void processUpRequest(Request request)
+    {
+        int startFloor = currentFloor;
+        // The elevator is not on the floor where the person has requested it i.e. source floor. So first bring it there.
+        if (startFloor < request.GetExternalRequest().GetSourceFloor())
         {
-            Status = State.STOPPED;
-            CurrentFloor = floor;
-            floorReady[floor] = false;
-            Console.WriteLine("Stopped at floor {0}", floor);
-        }
-
-        //Button to close the doors
-        private void CloseDoors(int floor)
-        {
-            CurrentFloor = floor;
-            floorReady[floor] = false;
-
-        }
-
-        private void MovingDown(int floor)
-        {
-            for (int i = CurrentFloor; i >= 1; i--)
+            for (int i = startFloor; i <= request.GetExternalRequest().GetSourceFloor(); i++)
             {
-                if (floorReady[i])
-                    Stop(floor);
+                try
+                {
+                    Thread.Sleep(1000);
+                }
+                catch (ThreadInterruptedException e)
+                {
+                    Console.WriteLine(e.ToString());
+                    Console.Write(e.StackTrace);
+                }
+                Console.WriteLine("We have reached floor -- " + i);
+                currentFloor = i;
+            }
+        }
+        // The elevator is now on the floor where the person has requested it i.e. source floor. User can enter and go to the destination floor.
+        Console.WriteLine("Reached Source Floor--opening door");
+        startFloor = currentFloor;
+        for (int i = startFloor + 1; i <= request.GetInternalRequest().GetDestinationFloor(); i++)
+        {
+            try
+            {
+                Thread.Sleep(1000);
+            }
+            catch (ThreadInterruptedException e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.Write(e.StackTrace);
+            }
+            Console.WriteLine("We have reached floor -- " + i);
+            currentFloor = i;
+            if (checkIfNewJobCanBeProcessed(request))
+            {
+                break;
+            }
+        }
+    }
+    public void processDownRequest(Request request)
+    {
+        int startFloor = currentFloor;
+        if (startFloor < request.GetExternalRequest().GetSourceFloor())
+        {
+            for (int i = startFloor; i <= request.GetExternalRequest().GetSourceFloor(); i++)
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+                }
+                catch (ThreadInterruptedException e)
+                {
+                    Console.WriteLine(e.ToString());
+                    Console.Write(e.StackTrace);
+                }
+                Console.WriteLine("We have reached floor -- " + i);
+                currentFloor = i;
+            }
+        }
+        Console.WriteLine("Reached Source Floor--opening door");
+
+        startFloor = currentFloor;
+
+        for (int i = startFloor; i >= request.GetInternalRequest().GetDestinationFloor(); i--)
+        {
+            try
+            {
+                Thread.Sleep(1000);
+            }
+            catch (ThreadInterruptedException e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.Write(e.StackTrace);
+            }
+            Console.WriteLine("We have reached floor -- " + i);
+            currentFloor = i;
+            if (checkIfNewJobCanBeProcessed(request))
+            {
+                break;
+            }
+        }
+    }
+    public bool checkIfNewJobCanBeProcessed(Request currentRequest)
+    {
+        if (checkIfJob())
+        {
+            if (currentDirection == Direction.UP)
+            {
+                Request request = currentJobs.Pop();
+                if (request.GetInternalRequest().GetDestinationFloor() < currentRequest.GetInternalRequest().GetDestinationFloor())
+                {
+                    currentJobs.Push(request);
+                    currentJobs.Push(currentRequest);
+                    return true;
+                }
+                currentJobs.Push(request);
+            }
+            if (currentDirection == Direction.DOWN)
+            {
+                Request request = currentJobs.Pop();
+                if (request.GetInternalRequest().GetDestinationFloor() > currentRequest.GetInternalRequest().GetDestinationFloor())
+                {
+                    currentJobs.Push(request);
+                    currentJobs.Push(currentRequest);
+                    return true;
+                }
+                currentJobs.Push(request);
+            }
+        }
+        return false;
+    }
+    public void addPendingDownJobsToCurrentJobs()
+    {
+        if (downPendingJobs.Count != 0)
+        {
+            Console.WriteLine("Pick a pending down job and execute it by putting in current job");
+            currentJobs = downPendingJobs;
+            currentDirection = Direction.DOWN;
+        }
+        else
+        {
+            currentState = State.IDLE;
+            Console.WriteLine("The elevator is in Idle state");
+        }
+    }
+    public void addPendingUpJobsToCurrentJobs()
+    {
+        if (upPendingJobs.Count != 0)
+        {
+            Console.WriteLine("Pick a pending up job and execute it by putting in current job");
+
+            currentJobs = upPendingJobs;
+            currentDirection = Direction.UP;
+        }
+        else
+        {
+            currentState = State.IDLE;
+            Console.WriteLine("The elevator is in Idle state");
+        }
+    }
+    public void addJob(Request request)
+    {
+        if (currentState == State.IDLE)
+        {
+            currentState = State.MOVING;
+            currentDirection = request.GetExternalRequest().GetDirectionToGo();
+            currentJobs.Push(request);
+        }
+        else if (currentState == State.MOVING)
+        {
+            if (request.GetExternalRequest().GetDirectionToGo() != currentDirection)
+            {
+                addtoPendingJobs(request);
+            }
+            else if (request.GetExternalRequest().GetDirectionToGo() == currentDirection)
+            {
+                if (currentDirection == Direction.UP && request.GetInternalRequest().GetDestinationFloor() < currentFloor)
+                {
+                    addtoPendingJobs(request);
+                }
+                else if (currentDirection == Direction.DOWN && request.GetInternalRequest().GetDestinationFloor() > currentFloor)
+                {
+                    addtoPendingJobs(request);
+                }
                 else
-                    continue;
+                {
+                    currentJobs.Push(request);
+                }
             }
-
-            Status = State.STOPPED;
-            Console.WriteLine("Please Wait until the door is opened.");
-            Console.WriteLine("The door is Opened.");
         }
-
-        private void MovingUp(int floor)
+    }
+    public void addtoPendingJobs(Request request)
+    {
+        if (request.GetExternalRequest().GetDirectionToGo() == Direction.UP)
         {
-            for (int i = CurrentFloor; i <= topfloor; i++)
-            {
-                if (floorReady[i])
-                    Stop(floor);
-                else
-                    continue;
-            }
-
-            Status = State.STOPPED;
-            Console.WriteLine("Please Wait until the door is opened.");
-            Console.WriteLine("The door is Opened.");
+            Console.WriteLine("Add to pending up jobs");
+            upPendingJobs.Push(request);
         }
-
-        void StayPut()
+        else
         {
-            Console.WriteLine("That's our current floor");
-        }
-
-        public void FloorPress(int floor)
-        {
-            if (floor > topfloor)
-            {
-                Console.WriteLine("We only have {0} floors", topfloor);
-                return;
-            }
-
-            floorReady[floor] = true;
-
-            switch (Status)
-            {
-
-                case State.DOWN:
-                    MovingDown(floor);
-                    break;
-
-                case State.STOPPED:
-                    if (CurrentFloor < floor)
-                        MovingUp(floor);
-                    else if (CurrentFloor == floor)
-                        StayPut();
-                    else
-                        MovingDown(floor);
-                    break;
-
-                case State.UP:
-                    MovingUp(floor);
-                    break;
-
-                default:
-                    break;
-            }
-
-
-        }
-
-        public enum State
-        {
-            UP,
-            STOPPED,
-            DOWN
+            Console.WriteLine("Add to pending down jobs");
+            downPendingJobs.Push(request);
         }
     }
 }
+public enum State
+{
+    MOVING,
+    STOPPED,
+    IDLE
+}
+public enum Direction
+{
+    UP,
+    DOWN
+}
+public class Request : IComparable<Request>
+{
+    public InternalRequest _internalRequest;
+    public ExternalRequest _externalRequest;
+    public Request(InternalRequest internalRequest, ExternalRequest externalRequest)
+    {
+        this._internalRequest = internalRequest;
+        this._externalRequest = externalRequest;
+    }
+    public InternalRequest GetInternalRequest()
+    {
+        return this._internalRequest;
+    }
+    public void SetInternalRequest(InternalRequest internalRequest)
+    {
+        this._internalRequest = internalRequest;
+    }
+    public ExternalRequest GetExternalRequest()
+    {
+        return this._externalRequest;
+    }
+    public void SetExternalRequest(ExternalRequest externalRequest)
+    {
+
+        this._externalRequest = externalRequest;
+    }
+    public int CompareTo(Request obj)
+    {
+        if (this._internalRequest.DestinationFloor > obj._internalRequest.DestinationFloor)
+            return 1;
+        else if (this._internalRequest.DestinationFloor < obj._internalRequest.DestinationFloor)
+            return -1;
+        else
+            return 0;
+    }
+
+}
+public class ProcessJobWorker
+{
+    private Elevator elevator;
+    public ProcessJobWorker(Elevator elevator)
+    {
+        this.elevator = elevator;
+    }
+
+    public void run()
+    {
+        elevator.startElevator();
+    }
+
+}
+public class AddJobWorker
+{
+
+    private Elevator elevator;
+    private Request request;
+
+    public AddJobWorker(Elevator elevator, Request request)
+    {
+        this.elevator = elevator;
+        this.request = request;
+    }
+
+    public void run()
+    {
+        try
+        {
+            Thread.Sleep(1000);
+        }
+        catch (ThreadInterruptedException e)
+        {
+            Console.WriteLine(e.ToString());
+            Console.Write(e.StackTrace);
+        }
+        elevator.addJob(request);
+    }
+
+}
+public class ExternalRequest
+{
+
+    private Direction _directionToGo;
+    private int _sourceFloor;
+
+    public ExternalRequest(Direction directionToGo, int sourceFloor)
+    {
+        this._directionToGo = directionToGo;
+        this._sourceFloor = sourceFloor;
+    }
+    public Direction GetDirectionToGo()
+    {
+        return this._directionToGo;
+    }
+    public void SetDirectionToGo(Direction directionToGo)
+    {
+        this._directionToGo = directionToGo;
+    }
+
+    public int GetSourceFloor()
+    {
+        return this._sourceFloor;
+    }
+    public void SetSourceFloor(int SourceFloor)
+    {
+        this._sourceFloor = SourceFloor;
+    }
+
+    public override string ToString()
+    {
+        return " The Elevator has been requested on floor - " + _sourceFloor + " and the person wants go in the - " + _directionToGo;
+    }
+
+}
+public class InternalRequest
+{
+    public int DestinationFloor;
+    public InternalRequest(int destinationFloor)
+    {
+        this.DestinationFloor = destinationFloor;
+    }
+    public int GetDestinationFloor()
+    {
+        return this.DestinationFloor;
+    }
+    public void SetDestinationFloor(int destinationFloor)
+
+    {
+        this.DestinationFloor = destinationFloor;
+    }
+    public override string ToString()
+    {
+        return "The destinationFloor is - " + DestinationFloor;
+    }
+
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Elevator elevator = new Elevator();
+        ProcessJobWorker processJobWorker = new ProcessJobWorker(elevator);
+        Thread t2 = new Thread(processJobWorker.run);
+        t2.Start();
+
+        try
+        {
+            Thread.Sleep(5000);
+        }
+        catch (ThreadInterruptedException e)
+        {
+            Console.WriteLine(e.ToString());
+            Console.Write(e.StackTrace);
+        }
+
+        ExternalRequest er = new ExternalRequest(Direction.UP, 2);
+
+        InternalRequest ir = new InternalRequest(5);
+
+        Request request1 = new Request(ir, er);
+        (new Thread(new AddJobWorker(elevator, request1).run)).Start();
+
+        try
+        {
+            Thread.Sleep(5000);
+        }
+        catch (ThreadInterruptedException e)
+        {
+            Console.WriteLine(e.ToString());
+            Console.Write(e.StackTrace);
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
